cc7e54e86017b9b10bc998ef6db2b7d270bb29b4
Stephanie Mizelle <samizelle@gmail.com>
2019-01-02 14:19:52 -0500

    initial commit2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
